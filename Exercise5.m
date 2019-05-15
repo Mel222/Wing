d6b73a025b172642bff5313c002d26072085f7f6
Mel222 <mja77@cam.ac.uk>
2019-05-15 12:11:40 +0100

    Circ

--- Exercise5.m
+++ Exercise5.m
@@ -9,45 +9,26 @@ xs=cos(theta);    %array of x coordinates
 
 ys=sin(theta);  %array of y coordinates
 
-
-% nx = 51;
-% 
-% ny = 41;
-% 
-% xmin = -2.5;
-% 
-% ymin = -2;
-% 
-% xmax = 2.5;
-% 
-% ymax = 2;
-% 
-% 
-% 
-% xm = zeros(nx,ny);
-% 
-% ym = zeros(nx,ny);
-% 
-% 
-% 
-% for i=1:nx
-% 
-%     for j=1:ny
-% 
-%         xm(i,j) = xmin + (i-1)*(xmax-xmin)/(nx-1);
-% 
-%         ym(i,j) = ymin + (j-1)*(ymax-ymin)/(ny-1);
-% 
-% 
-%     end    
-% 
-% end
-
 alpha=0;
-A = build_lhs(xs,ys);
-b = build_rhs(xs,ys,alpha);
-gamma = A\b;
+A0 = build_lhs(xs,ys);
+b0 = build_rhs(xs,ys,alpha);
+gamma0 = A0\b0;
 
 figure(10)
-plot(theta/pi, gamma)
+plot(theta/pi, gamma0)
 axis([0 2 -2.5 2.5])
+
+alpha=0.1;
+A1 = build_lhs(xs,ys);
+b1 = build_rhs(xs,ys,alpha);
+gamma1 = A1\b1;
+
+figure(11)
+plot(theta/pi, gamma1)
+axis([0 2 -2.5 2.5])
+
+Circ0 = sum(gamma0)*theta(2)
+Circ1 = sum(gamma1)*theta(2)
+
+CircTheo = -4*pi*sin(alpha)
+
